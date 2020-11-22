6be24243c5df8432f700f0cf378ca29c0df91f5c
benihime91 <aburagohain160@gmail.com>
2020-11-22 12:44:12 +0530

    add/update inference notebook and script from kaggle

--- inference.py
+++ inference.py
@@ -5,6 +5,7 @@ from torchvision import models
 from typing import Union
 import cv2
 from tqdm.auto import tqdm
+import numpy as np
 
 
 class InferenceDs(torch.utils.data.Dataset):
@@ -32,42 +33,30 @@ class InferenceModel(nn.Module):
         self.base = base
 
     def forward(self, x):
-        return self.base(self.classifier(x))
+        features = self.classifier(x)
+        logits = self.base(features)
+        return logits
 
 
 class Predictor:
     def __init__(self, model:nn.Module, device:Union[str, torch.device]):
         self.model = model
         self.device = device
-
-        if self.model.device != device:
-            self.model.to(device)
+        self.model.to(device)
         self.model.eval()
-
-    def _predict_fn(self, batch):
-        image, unique_idx = batch
-        
-        image = image.to(self.device)
-        
-        logits = self.model(image)
-        preds  = torch.argmax(logits, -1)
-        preds  = preds.cpu().detach().numpy()
-        return list(preds), list(unique_idx)
-
+ 
     def predict(self, dataloader:torch.utils.data.DataLoader):
         batch_preds = []
         batch_idxs  = []
 
-        for batch in tqdm(dataloader):
-            preds, unique_idx = _predict_fn(batch)
-            batch_preds = batch_preds + preds
-            batch_idxs = batch_idxs + unique_idx
+        for batch in tqdm(dataloader, leave=False):
+            image, unique_idx = batch
+            image = image.to(self.device)
 
+            logits = self.model(image)
+            
+            batch_preds += [torch.softmax(logits, 1).detach().cpu().numpy()]
+            batch_idxs = batch_idxs + list(unique_idx)
+
+        batch_preds = np.concatenate(batch_preds, axis=0)
         return batch_preds, batch_idxs
-
-
-
-
-
-
-
