9d65484ccd95a91241bfd49d4e48e6f366a26f98
benihime91 <aburagohain160@gmail.com>
2020-11-23 13:01:43 +0530

    add docs

--- hydra_run.py
+++ hydra_run.py
@@ -1,4 +1,14 @@
 if __name__ == '__main__':
+    """
+    Use this script to run train the pytorch-lightning model
+    To run this script we need to pass in two arguments to the 
+    command line while running this script
+
+    To run:
+    python hydra_run.py \
+        --config_dir {path to the main config directory} \
+        --config_fname {the name of the config (usually the file name without the .yaml extension)}
+    """
     import argparse
     from hydra.experimental import compose, initialize_config_dir
     from experiment import run
@@ -23,9 +33,10 @@ if __name__ == '__main__':
     # parse given arguments
     args = parser.parse_args()
 
-    # Run pytorch-lightning model
     abs_config_dir = os.path.abspath(args.config_dir)
-    with initialize_config_dir(config_dir=abs_config_dir):
+    # initialize hydra config
+    with initialize_config_dir(config_dir=abs_config_dir, job_name="train_lightning_model"):
+        # Run pytorch-lightning model
         config = compose(config_name=args.config_fname)
         # run model training
         run(config)
