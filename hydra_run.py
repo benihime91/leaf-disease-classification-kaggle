38f23045df5ca8b21d2ec39738258772ff23200d
benihime91 <aburagohain160@gmail.com>
2020-11-27 11:37:38 +0530

    minor fixes to config and hydra_run.py

--- hydra_run.py
+++ hydra_run.py
@@ -6,12 +6,9 @@ from omegaconf import DictConfig
 from cutmix import run as cut_mix_run
 from experiment import run
 from mixup import run as mix_run
+import warnings
 
-__all__ = {
-    "experiment": run,
-    "mixup": mix_run,
-    "cutmix": cut_mix_run,
-}
+__all__ = {"experiment": run, "mixup": mix_run, "cutmix": cut_mix_run,}
 
 log = logging.getLogger(__name__)
 
@@ -19,7 +16,7 @@ log = logging.getLogger(__name__)
 @hydra.main(config_path="conf", config_name="config")
 def run_model(cfg: DictConfig) -> None:
     # extract the function to run from the config
-    log.info(f"train using {cfg.run}.py")
+    log.info(f"Init run function from {cfg.run}.py")
     run_fn = __all__[cfg.run]
     # run training job
     run_fn(cfg, logger=log)
@@ -34,4 +31,5 @@ if __name__ == "__main__":
     Usage :
         ! python hydra_run.py --config_path conf --config_name config
     """
+    warnings.filterwarnings("ignore")
     run_model()
