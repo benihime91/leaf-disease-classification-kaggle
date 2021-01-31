b6a61051fd9945adce2a14ce016002c9b3e63b11
benihime91 <aburagohain160@gmail.com>
2021-01-31 21:10:43 +0530

    New API to support recent developments

--- src/data/__init__.py
+++ src/data/__init__.py
@@ -1,3 +1,3 @@
-from .dataset_factory import CassavaClassificationModule
+from .dataset_factory import create_transform, DatasetMapper
 from .datasets import load_data, CassavaDataset
 from .mixmethods import BaseMixMethodHandler, Cutmix, Mixup, Snapmix
