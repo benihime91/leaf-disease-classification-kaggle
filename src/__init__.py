76f56c6f0ab110310069310ec38e71aaa0f28b5b
benihime91 <aburagohain160@gmail.com>
2021-01-31 22:25:05 +0530

    fort logging to loguru

--- src/__init__.py
+++ src/__init__.py
@@ -1,8 +1,8 @@
 __version__ = "0.0.2"
 
 from loguru import logger as _logger
+import sys
 
-# import sys
-# _format = "[<green>{time:MM/DD HH:mm:ss}</green> <magenta>{name}</magenta>]: {message}"
-# _logger.remove()
-# _logger.add(sys.stderr, colorize=True, format=_format)
+_format = "[<green>{time:MM/DD HH:mm:ss}</green> <magenta>{name}</magenta>]: {message}"
+_logger.remove()
+_logger.add(sys.stderr, colorize=True, format=_format)
