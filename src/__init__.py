df7cecfd407795922e4aedcb412542ec3490ee21
benihime91 <aburagohain160@gmail.com>
2021-02-01 15:01:54 +0530

    logging changes

--- src/__init__.py
+++ src/__init__.py
@@ -4,5 +4,5 @@ from loguru import logger as _logger
 import sys
 
 _logger.remove()
-format = "<green>{time:YYYY-MM-DD HH:mm:ss}</green> - <cyan>{name: <20}</cyan>: [<level>{level: <8}</level>] - {message}"
+format = "<green>{time:YYYY-MM-DD HH:mm:ss}</green> - <cyan>{name:}</cyan>: [<level>{level}</level>] - {message}"
 _logger.add(sys.stdout, format=format, colorize=True)
